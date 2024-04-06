2589476afde8582c4fc085e67d256ae3f89b2ed8
Lukasz Kosminski <48951121+lukaszkosminski@users.noreply.github.com>
2024-04-06 20:44:06 +0200

    added current-user endpoint and made changes in Spring Security configuration

--- my-fund-backend/src/main/java/com/myfund/config/SecurityConfig.java
+++ my-fund-backend/src/main/java/com/myfund/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.myfund.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 
 @Configuration
@@ -33,8 +36,21 @@ public class SecurityConfig {
                         })
                         .permitAll()
                 )
-                .logout(logout -> logout.permitAll())
-                .csrf(csrf -> csrf.disable());
+                .logout(logout -> logout
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            if (authentication != null) {
+                                response.setStatus(HttpServletResponse.SC_OK);
+                            } else {
+                                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            }
+                        })
+                        .permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                );
+
         return http.build();
     }
 }
