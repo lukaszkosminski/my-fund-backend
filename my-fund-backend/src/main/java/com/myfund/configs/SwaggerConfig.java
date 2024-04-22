7c741b648569772a8e4cd45d7513a8af7a43444c
Lukasz Kosminski <48951121+lukaszkosminski@users.noreply.github.com>
2024-04-22 17:05:34 +0200

    added version and build date to Swagger documentation

--- my-fund-backend/src/main/java/com/myfund/configs/SwaggerConfig.java
+++ my-fund-backend/src/main/java/com/myfund/configs/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
@@ -12,11 +13,22 @@ import org.springframework.web.filter.ForwardedHeaderFilter;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Swagger documentation", description = "My fund", version = "v1"))
+@OpenAPIDefinition()
 public class SwaggerConfig {
+
+    @Value("${app.version}")
+    String appVer;
+
+    @Value("${app.build.date}")
+    private String buildDate;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Swagger documentation")
+                        .description("My fund - Version: " + appVer + ", Build date: " + buildDate)
+                        .version("v1"))
                 .addSecurityItem(new SecurityRequirement().addList(AUTHORIZATION))
                 .components(new Components().addSecuritySchemes(AUTHORIZATION,
                         new io.swagger.v3.oas.models.security.SecurityScheme().type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
