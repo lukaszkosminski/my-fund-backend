48381d05e45d50a0e865636ce2ac39380f3ef0b5
Łukasz Kośmiński <polde@protonmail.com>
2024-08-01 17:30:14 +0200

    Remove frontend files

--- my-fund-frontend/src/app/components/topbar/app.topbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-topbar',
-  templateUrl: './app.topbar.component.html',
-})
-export class AppTopBarComponent {
-  constructor() {}
-}
--- my-fund-frontend/src/app/components/ui/form/form-row/form-row.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-form-row',
-  template: '<div class="mb-5"><ng-content></ng-content></div>',
-})
-export class FormRowComponent {}
